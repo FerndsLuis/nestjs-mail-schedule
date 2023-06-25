3b6432057c1b91d478e1b9c02284692b7ce70fe6
Luis <hsantos.luis@outlook.com>
2023-06-25 18:12:53 -0300

    feat: components nav and button

--- frontend/src/components/Nav/Nav.tsx
+++ frontend/src/components/Nav/Nav.tsx
@@ -1,9 +1,12 @@
+import { Button, NavLogo } from 'components';
+
 export function Nav() {
   return (
-    <nav>
-      <p>
-        Schedule<span>Mail</span>
-      </p>
+    <nav className="w-full h-20 bg-gray-100 border-b border-gray-200">
+      <div className="w-full h-full max-w-7xl m-auto flex items-center justify-between">
+        <NavLogo />
+        <Button variant="primary">Escrever Agora</Button>
+      </div>
     </nav>
   );
 }
